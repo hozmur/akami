463f564370fa1ca8d5dbc878b59c8f8608b4b097
Huseyin Gomleksizoglu <huseyin@gomleksizoglu.com>
2015-12-28 16:36:35 +0200

    order of xml format changed

--- lib/akami/wsse.rb
+++ lib/akami/wsse.rb
@@ -91,18 +91,18 @@ module Akami
 
     # Returns the XML for a WSSE header.
     def to_xml
-      if signature? and signature.have_document?
-        Gyoku.xml wsse_signature.merge!(hash)
+      if username_token?
+        Gyoku.xml wsse_username_token.merge!(hash)
+      elsif timestamp?
+        Gyoku.xml wsu_timestamp.merge!(hash)
       elsif username_token? && timestamp?
         Gyoku.xml wsu_timestamp.merge!(wsse_username_token) {
           |key, v1, v2| v1.merge!(v2) {
             |key, v1, v2| v1.merge!(v2)
           }
         }
-      elsif username_token?
-        Gyoku.xml wsse_username_token.merge!(hash)
-      elsif timestamp?
-        Gyoku.xml wsu_timestamp.merge!(hash)
+      elsif signature? and signature.have_document?
+        Gyoku.xml wsse_signature.merge!(hash)
       else
         ""
       end
